817ec8f20d582b5929a84c4eff4296c94ae28a01
SowntharyaSF4256 <147583440+SowntharyaSF4256@users.noreply.github.com>
2023-12-21 11:13:28 +0530

    load SfPulltoRefresh inside ScrollView with SfListView in . NET Maui

---
+++ RefreshListView/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using RefreshListView.Model;
+using Syncfusion.Maui.DataSource;
+using System.Collections.Specialized;
+
+namespace RefreshListView
+{
+    public partial class MainPage : ContentPage
+    {
+        SearchBar? searchBar = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            searchBar = sender as SearchBar;
+            if (listView.DataSource != null)
+            {
+                listView.DataSource.Filter = FilterContacts;
+                listView.DataSource.RefreshFilter();
+            }
+
+        }
+
+        private bool FilterContacts(object obj)
+        {
+            if (searchBar == null || searchBar.Text == null)
+                return true;
+            var inboxInfos = obj as InboxInfo;
+            if (inboxInfos.Name.ToLower().Contains(searchBar.Text.ToLower()) || inboxInfos.Subject.ToLower().Contains(searchBar.Text.ToLower()) || inboxInfos.Description.ToLower().Contains(searchBar.Text.ToLower()))
+            {
+                return true;                
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
